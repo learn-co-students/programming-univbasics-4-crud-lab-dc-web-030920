9a56b093a9a9f4044da8763a0771cadfcece1583
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-05 20:29:46 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,26 +19,31 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop()
-  p array
+  removed_element = array.pop()
+  p removed_element
 end
 
 def remove_element_from_start_of_array(array)
-
+  removed_element = array.shift()
+  p removed_element
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  name_of_element = array[index_number]
+  p name_of_element
 end
 
 def retrieve_first_element_from_array(array)
-
+  first_element = array[0]
+  p first_element
 end
 
 def retrieve_last_element_from_array(array)
-
+  last_element = array[array.length - 1]
+  p last_element
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array[index_number] = element
+  p element
 end
